7aab28cae2f3809d85b5731b855f0ebdb1fa1558
muskan-singh95 <141938171+muskan-singh95@users.noreply.github.com>
2023-08-10 20:11:57 +0530

    Add files via upload

---
+++ backend/app.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const helmet = require("helmet");
+const dotenv = require('dotenv');
+dotenv.config();
+require('dotenv').config({ path: 'final-env-file.env' });
+
+const _ = require("lodash");
+const bodyParser = require("body-parser");
+var cors = require("cors");
+const uri = "mongodb://localhost:27017/test";
+const mongoose = require("mongoose");
+const port = 3000;
+const routes = require("./Routes/routes");
+const getRoutes = require("./Routes/get");
+const postRoutes = require("./Routes/post");
+const putRoutes = require("./Routes/put");
+// const userRoutes = require("./Routes/user");
+
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    const app = express();
+    app.use(helmet.hidePoweredBy());
+    app.use(bodyParser.json({limit: '50mb'}));
+    app.use(cors());
+
+    app.use("/api/get", getRoutes);
+    app.use("/api/post", postRoutes);
+    app.use("/api/put", putRoutes);
+    // app.use("/api", routes);
+    app.get("/server/status", (req, res) => {
+      res.status(200).send("Server OK");
+    });
+    app.use(function (req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+      );
+      next();
+    });
+
+    app.listen(port, () => {
+      console.log(`Server started at Port No: ${port}`);
+    });
+  });
